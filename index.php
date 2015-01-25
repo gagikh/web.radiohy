d3da06b7e0de20e602914d08dcf7b043748ee4c0
djgago@yandex.ru <hakgagik@yahoo.com>
2015-01-25 13:56:14 +0400

    - js load.

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
     file_put_contents("backup.json", $stations);
 
     $repo = "https://raw.githubusercontent.com/djgago/web.radiohy/master/";
+    $js_file_name   = "stations.js";
     $css_file_name  = "stations.css";
     $html_file_name = "stations.html";
+
+    $url = $repo . $js_file_name;
+    $js = file_get_contents($url);
+    file_put_contents($js_file_name, $js);
 
     $url = $repo . $css_file_name;
     $css = file_get_contents($url);
